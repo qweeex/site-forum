1f25df96b723f600bfb07fabdeff80357897a358
Arkady Pugachev <qweeex@yandex.com>
2020-01-14 17:54:37 +0300

    Add shop

--- js/main.js
+++ js/main.js
@@ -32,4 +32,32 @@ $(document).ready(() => {
     $('.header-menu').toggleClass('header-menu__active');
   });
 
+
+  // Stock sort btn
+
+  // Btn all card
+  $('.sort-all').on('click', (e) => {
+    $('.sort-btn').removeClass('sort-active');
+    $('.sort-all').addClass('sort-active');
+    $('.card-event, .card-stock').show(500);
+  });
+
+  // Btn stock card
+  $('.sort-stock').on('click', (e) => {
+    e.preventDefault();
+    $('.sort-btn').removeClass('sort-active');
+    $('.sort-stock').addClass('sort-active');
+    $('.card-stock').show(500);
+    $('.card-event').hide(500);
+  });
+
+  // Btn event card
+  $('.sort-event').on('click', (e) => {
+    e.preventDefault();
+    $('.sort-btn').removeClass('sort-active');
+    $('.sort-event').addClass('sort-active');
+    $('.card-stock').hide(500);
+    $('.card-event').show(500);
+  });
+
 });
